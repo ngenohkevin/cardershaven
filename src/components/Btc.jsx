2ec10364f6c813cbeff062d71c6b728966b6c04b
Kevin Ngenoh <kevin@Kevin>
2022-11-21 22:54:13 +0300

    updated payment

--- src/components/Btc.jsx
+++ src/components/Btc.jsx
@@ -18,16 +18,19 @@ const Btc = () => {
                     name="browserRedirect"
                     defaultValue="https://cardershaven.cc/"
                 />
+                <input type="hidden" name="price" defaultValue={60} />
                 <input type="hidden" name="currency" defaultValue="USD" />
+                <input type="hidden" name="defaultPaymentMethod" defaultValue="BTC" />
                 <input
                     type="image"
                     className="submit"
                     name="submit"
                     src="https://cdn.iconscout.com/icon/free/png-512/bitcoin-390-920575.png"
                     style={{ width: 146 }}
-                    alt=" Payment Processor"
+                    alt="Pay with BTCPay Server, a Self-Hosted Bitcoin Payment Processor"
                 />
             </form>
+
 
         </div>
     );
