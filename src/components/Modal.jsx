911ab739776ac992e045aef7f41588f02274dcc1
Kevin Ngenoh <35255853+ngenohkevin@users.noreply.github.com>
2022-11-26 20:16:55 +0300

    ch

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,68 +12,85 @@ const Modal = ({visible, onClose}) => {
 
     if(!visible) return null
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center">
-            <div className="w-[600px] flex flex-col">
-                <button className="text-white text-xl place-self-end" onClick={handleOnClose}>X</button>
-                <div className="bg-gray-300 bg-gradient-to-r from-blue-200 p-2 rounded">
-                    <div className="py-6 px-6 lg:px-8 text-left">
-                        <h3 className="mb-4 text-xl text-gray-900 text-center font-bold">
-                            After Payment a link will be sent to your email to login and Access full features of our
-                            forum
-                        </h3>
-                        <hr/>
-                        <div className="flex justify-center items-center text-sky-700 italic">
-                            <ul className="list-disc">
-                                <li>The higher you pay the more services and offers you'll access</li>
-                                <li>30% bonus on $1000+ deposit</li>
-                                <li>Access to CC checkers for Free! on $200+ deposit</li>
-                                <li>Your balance will be added to your account after 3 confirmations</li>
-                                <li>You can withdraw your funds any time you want</li>
-                                <li>Access to all bank logs</li>
-                                <li>Full Support from our staff and many more!</li>
-                            </ul>
-                        </div>
-                        <br/>
-                        {/*        <form className="space-y-6" onSubmit={handleSubmit}>*/}
-                        {/*            <div>*/}
-                        {/*                <label*/}
-                        {/*                    htmlFor="email"*/}
-                        {/*                    className="block mb-2 text-sm font-medium text-gray-900"*/}
-                        {/*                >*/}
-                        {/*                    Your Email*/}
-                        {/*                </label>*/}
-                        {/*                <input*/}
-                        {/*                    type="email"*/}
-                        {/*                    name="email"*/}
-                        {/*                    id="email"*/}
-                        {/*                    className="bg-gray-200 border border-gray-300 text-gray-800 text-sm rounded-lg*/}
-                        {/*                    focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"*/}
-                        {/*                    placeholder="name@company.com"*/}
-                        {/*                    required*/}
-                        {/*                />*/}
-                        {/*            </div>*/}
-                        {/*            <div className="text-center">*/}
-                        {/*                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none*/}
-                        {/*font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5"*/}
+      <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center">
+        <div className="w-[600px] flex flex-col">
+          <button
+            className="text-white text-xl place-self-end"
+            onClick={handleOnClose}
+          >
+            X
+          </button>
+          <div className="bg-gray-300 bg-gradient-to-r from-blue-200 p-2 rounded">
+            <div className="py-6 px-6 lg:px-8 text-left">
+              <h3 className="mb-4 text-xl text-gray-900 text-center font-bold">
+                After Payment a link will be sent to your email to login and
+                Access full features of our forum
+              </h3>
+              <hr />
+              <div className="flex justify-center items-center text-sky-700 italic">
+                <ul className="list-disc">
+                  <li>
+                    The higher you pay the more services and offers you'll
+                    access
+                  </li>
+                  <li>30% bonus on $1000+ deposit</li>
+                  <li>Access to CC checkers for Free! on $200+ deposit</li>
+                  <li>
+                    Your balance will be added to your account after 3
+                    confirmations
+                  </li>
+                  <li>You can withdraw your funds any time you want</li>
+                  <li>Access to all bank logs</li>
+                  <li>Full Support from our staff and many more!</li>
+                </ul>
+              </div>
+              <br />
+              {/*        <form className="space-y-6" onSubmit={handleSubmit}>*/}
+              {/*            <div>*/}
+              {/*                <label*/}
+              {/*                    htmlFor="email"*/}
+              {/*                    className="block mb-2 text-sm font-medium text-gray-900"*/}
+              {/*                >*/}
+              {/*                    Your Email*/}
+              {/*                </label>*/}
+              {/*                <input*/}
+              {/*                    type="email"*/}
+              {/*                    name="email"*/}
+              {/*                    id="email"*/}
+              {/*                    className="bg-gray-200 border border-gray-300 text-gray-800 text-sm rounded-lg*/}
+              {/*                    focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"*/}
+              {/*                    placeholder="name@company.com"*/}
+              {/*                    required*/}
+              {/*                />*/}
+              {/*            </div>*/}
+              {/*            <div className="text-center">*/}
+              {/*                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none*/}
+              {/*font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5"*/}
 
-                        {/*                >*/}
-                        {/*                    <a href="https://www.blockonomics.co/pay-url/c9c0ec04e8034696" target="_blank" rel="noopener noreferrer" style={{color: "white"}}>*/}
-                        {/*                        Continue*/}
-                        {/*                    </a>*/}
-                        {/*                </button>*/}
-                        {/*            </div>*/}
-                        {/*        </form>*/}
-                        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none
-                font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">
-                            <a href="https://www.blockonomics.co/pay-url/b624e5fc734546f2" target="_blank" rel="noopener noreferrer" style={{color: "white"}}>
-                                Continue to payment
-                            </a>
-
-                        </button>
-                    </div>
-                </div>
+              {/*                >*/}
+              {/*                    <a href="https://www.blockonomics.co/pay-url/c9c0ec04e8034696" target="_blank" rel="noopener noreferrer" style={{color: "white"}}>*/}
+              {/*                        Continue*/}
+              {/*                    </a>*/}
+              {/*                </button>*/}
+              {/*            </div>*/}
+              {/*        </form>*/}
+              <button
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none
+                font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5"
+              >
+                <a
+                  href="https://www.blockonomics.co/pay-url/c9c0ec04e8034696"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "white" }}
+                >
+                  Continue to payment
+                </a>
+              </button>
             </div>
+          </div>
         </div>
+      </div>
     );
 };
 
